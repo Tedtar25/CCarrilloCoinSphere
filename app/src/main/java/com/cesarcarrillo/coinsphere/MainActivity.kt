657cbf7debc7a4dea18cc6d5e83ffbe71f166038
Tedtar25 <cesar.carrillo69@hotmail.com>
2025-09-13 12:29:11 -0600

    V1.3 - Mockup Completo, Logos Lado Izquierod

--- app/src/main/java/com/cesarcarrillo/coinsphere/MainActivity.kt
+++ app/src/main/java/com/cesarcarrillo/coinsphere/MainActivity.kt
@@ -11,12 +11,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,23 +25,30 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.cesarcarrillo.coinsphere.ui.theme.CoinSphereTheme
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.layout.ContentScale
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            app()
+            App()
         }
     }
 }
 
 @Preview
 @Composable
-fun app() {
+fun App() {
     LazyColumn(
         modifier = Modifier
+            .statusBarsPadding()
             .fillMaxSize()
             .background(Color(0xFF1A1D2E))
     ) {
@@ -145,24 +153,25 @@ fun app() {
                 horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
                     .fillMaxWidth()
+                    //.width(60.dp)
                     .padding(15.dp)
             ) {
                 item {
                     Text(
-                        text = "#    Name                                                      Price",
+                        text = "#    Name",
                         fontSize = 14.sp,
                         color = Color.Gray
                     )
                 }
-                    /*
-                item {
-                Text(
-                        text = "Price",
-                        fontSize = 16.sp,
-                        color = Color.Gray
-                    )
-                }
-                */
+
+            item {
+            Text(
+                    text = "Price",
+                    fontSize = 16.sp,
+                    color = Color.Gray
+                )
+            }
+
 
             }
         }
@@ -183,11 +192,26 @@ fun app() {
                         .fillMaxWidth()
                         .padding(16.dp)
                 ) {
-                    Text(
-                        text = "1. Bitcoin (BTC)",
-                        fontSize = 14.sp,
-                        color = Color.White
-                    )
+                    Row {
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/128/color/btc.png")
+                                .crossfade(true)
+                                .build(),
+                            contentDescription = "Logo Bitcoin",
+                            modifier = Modifier
+                                .size(28.dp)
+                                .clip(CircleShape),
+                            contentScale = ContentScale.Crop
+                        )
+
+                        Text(
+                            text = "1. Bitcoin",
+                            fontSize = 14.sp,
+                            color = Color.White,
+                            modifier = Modifier.padding(start = 8.dp)
+                        )
+                    }
 
                     Text(
                         text = "$27,000",
@@ -215,11 +239,26 @@ fun app() {
                         .fillMaxWidth()
                         .padding(16.dp)
                 ) {
-                    Text(
-                        text = "2. Bitcoin (BTC)",
-                        fontSize = 14.sp,
-                        color = Color.White
-                    )
+                    Row {
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/128/color/eth.png")
+                                .crossfade(true)
+                                .build(),
+                            contentDescription = "Logo Ethereum",
+                            modifier = Modifier
+                                .size(28.dp)
+                                .clip(CircleShape),
+                            contentScale = ContentScale.Crop
+                        )
+
+                        Text(
+                            text = "2. Ethereum",
+                            fontSize = 14.sp,
+                            color = Color.White,
+                            modifier = Modifier.padding(start = 8.dp)
+                        )
+                    }
 
                     Text(
                         text = "$27,000",
@@ -247,11 +286,26 @@ fun app() {
                         .fillMaxWidth()
                         .padding(16.dp)
                 ) {
-                    Text(
-                        text = "3. Bitcoin (BTC)",
-                        fontSize = 14.sp,
-                        color = Color.White
-                    )
+                    Row {
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/128/color/usdt.png")
+                                .crossfade(true)
+                                .build(),
+                            contentDescription = "Logo Tether",
+                            modifier = Modifier
+                                .size(28.dp)
+                                .clip(CircleShape),
+                            contentScale = ContentScale.Crop
+                        )
+
+                        Text(
+                            text = "3. Tether",
+                            fontSize = 14.sp,
+                            color = Color.White,
+                            modifier = Modifier.padding(start = 8.dp)
+                        )
+                    }
 
                     Text(
                         text = "$27,000",
@@ -279,11 +333,26 @@ fun app() {
                         .fillMaxWidth()
                         .padding(16.dp)
                 ) {
-                    Text(
-                        text = "4. Bitcoin (BTC)",
-                        fontSize = 14.sp,
-                        color = Color.White
-                    )
+                    Row {
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/128/color/bnb.png")
+                                .crossfade(true)
+                                .build(),
+                            contentDescription = "Logo XRP",
+                            modifier = Modifier
+                                .size(28.dp)
+                                .clip(CircleShape),
+                            contentScale = ContentScale.Crop
+                        )
+
+                        Text(
+                            text = "4. XRP",
+                            fontSize = 14.sp,
+                            color = Color.White,
+                            modifier = Modifier.padding(start = 8.dp)
+                        )
+                    }
 
                     Text(
                         text = "$27,000",
@@ -311,11 +380,26 @@ fun app() {
                         .fillMaxWidth()
                         .padding(16.dp)
                 ) {
-                    Text(
-                        text = "5. Bitcoin (BTC)",
-                        fontSize = 14.sp,
-                        color = Color.White
-                    )
+                    Row {
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/128/color/xrp.png")
+                                .crossfade(true)
+                                .build(),
+                            contentDescription = "Logo BNB",
+                            modifier = Modifier
+                                .size(28.dp)
+                                .clip(CircleShape),
+                            contentScale = ContentScale.Crop
+                        )
+
+                        Text(
+                            text = "5. BNB",
+                            fontSize = 14.sp,
+                            color = Color.White,
+                            modifier = Modifier.padding(start = 8.dp)
+                        )
+                    }
 
                     Text(
                         text = "$27,000",
@@ -343,75 +427,26 @@ fun app() {
                         .fillMaxWidth()
                         .padding(16.dp)
                 ) {
-                    Text(
-                        text = "6. Bitcoin (BTC)",
-                        fontSize = 14.sp,
-                        color = Color.White
-                    )
+                    Row {
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/128/color/ada.png")
+                                .crossfade(true)
+                                .build(),
+                            contentDescription = "Logo Solana",
+                            modifier = Modifier
+                                .size(28.dp)
+                                .clip(CircleShape),
+                            contentScale = ContentScale.Crop
+                        )
 
-                    Text(
-                        text = "$27,000",
-                        fontSize = 14.sp,
-                        color = Color.White
-                    )
-                }
-
-            }
-        }
-
-        item {
-            Card(
-                shape = RoundedCornerShape(16.dp),
-                colors = CardDefaults.cardColors(
-                    containerColor = Color(0xFF2A2E42)
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-            ) {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                ) {
-                    Text(
-                        text = "7. Bitcoin (BTC)",
-                        fontSize = 14.sp,
-                        color = Color.White
-                    )
-
-                    Text(
-                        text = "$27,000",
-                        fontSize = 14.sp,
-                        color = Color.White
-                    )
-                }
-
-            }
-        }
-
-        item {
-            Card(
-                shape = RoundedCornerShape(16.dp),
-                colors = CardDefaults.cardColors(
-                    containerColor = Color(0xFF2A2E42)
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-            ) {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                ) {
-                    Text(
-                        text = "8. Bitcoin (BTC)",
-                        fontSize = 14.sp,
-                        color = Color.White
-                    )
+                        Text(
+                            text = "6. Solana",
+                            fontSize = 14.sp,
+                            color = Color.White,
+                            modifier = Modifier.padding(start = 8.dp)
+                        )
+                    }
 
                     Text(
                         text = "$27,000",
